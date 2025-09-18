05c8cdaec37d24261252dfaf3d368fdfad969279
mrrobotcrypto <hsn_dyn721@hotmail.com>
2025-09-18 15:16:21 +0300

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,9 @@
-// postcss.config.mjs
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+import tailwind from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: [tailwind, autoprefixer],
 };
+
+export default config;
