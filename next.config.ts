91a62b1aef7ec9c23a0eecb5f451291f756285fe
mrrobotcrypto <hsn_dyn721@hotmail.com>
2025-09-18 14:58:24 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-  experimental: { appDir: true },
-  images: { unoptimized: true },
+  eslint: {
+    ignoreDuringBuilds: true, // Lint hataları build’i durdurmasın
+  },
+  typescript: {
+    ignoreBuildErrors: true, // TS hataları build’i durdurmasın
+  },
+  experimental: {
+    appDir: true, // App Router açık
+  },
+  images: {
+    unoptimized: true, // Vercel’de optimize etme zorunluluğunu kapat
+  },
 };
 
 export default nextConfig;
