ef5cfdfd01db023f4e8f32e53fb3b2b493a72e9b
mrrobotcrypto <hsn_dyn721@hotmail.com>
2025-09-18 14:28:03 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div style={{ 
-      display: "flex", 
-      alignItems: "center", 
-      justifyContent: "center", 
-      height: "100vh", 
-      fontSize: "2rem",
-      fontWeight: "bold"
-    }}>
-      Hello World 🚀
+    <div>
+      <h1>Hello with Images</h1>
+      <Image src="/next.svg" alt="Next logo" width={100} height={100} />
+      <Image src="/vercel.svg" alt="Vercel logo" width={100} height={100} />
     </div>
   );
 }
